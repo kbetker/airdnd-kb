3f0f7ace99cbee2e8e5457157afd9138f4fe1a47
kbetker <kevin@betker.org>
2021-05-20 09:00:48 -0500

    working on authentication

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const app = express();
 const morgan = require('morgan');
 const cors = require('cors');
 const csurf = require('csurf');
@@ -7,8 +8,8 @@ const cookieParser = require('cookie-parser');
 
 const { environment } = require('./config');
 const isProduction = environment === 'production';
+const routes = require('./routes');
 
-const app = express();
 
 app.use(morgan('dev'));
 app.use(cookieParser());
@@ -17,29 +18,61 @@ app.use(express.json());
 
 // Security Middleware
 if (!isProduction) {
-    app.use(cors());
-  }
-  app.use(helmet({
-    contentSecurityPolicy: false
-  }));
+  app.use(cors());
+}
+app.use(helmet({
+  contentSecurityPolicy: false
+}));
 
-  // Set the _csrf token and create req.csrfToken method
-  app.use(
-    csurf({
-      cookie: {
-        secure: isProduction,
-        sameSite: isProduction && "Lax",
-        httpOnly: true,
-      },
-    })
+// Set the _csrf token and create req.csrfToken method
+app.use(
+  csurf({
+    cookie: {
+      secure: isProduction,
+      sameSite: isProduction && "Lax",
+      httpOnly: true,
+    },
+  })
   );
-
-
-  const routes = require('./routes');
 
   app.use(routes);
 
 
+
+  app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = ["The requested resource couldn't be found."];
+    err.status = 404;
+    next(err);
+  });
+
+
+  const { ValidationError } = require('sequelize');
+
+// ...
+
+// Process sequelize errors
+app.use((err, _req, _res, next) => {
+  // check if error is a Sequelize error:
+  if (err instanceof ValidationError) {
+    err.errors = err.errors.map((e) => e.message);
+    err.title = 'Validation error';
+  }
+  next(err);
+});
+
+
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    title: err.title || 'Server Error',
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack,
+  });
+});
 
 
   module.exports = app;
