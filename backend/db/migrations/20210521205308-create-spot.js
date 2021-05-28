2bad1691ae633d31c3844db521736fef76e7e0c9
kbetker <kevin@betker.org>
2021-05-28 07:46:18 -0500

    updated seeders and migrations

--- backend/db/migrations/20210521205308-create-spot.js
+++ backend/db/migrations/20210521205308-create-spot.js
@@ -33,7 +33,7 @@ module.exports = {
         type: Sequelize.TEXT
       },
       region: {
-        allowNull: false,
+        allowNull: true,
         type: Sequelize.STRING,
         default: 'Sword Coast'
       },
