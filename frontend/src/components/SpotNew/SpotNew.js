affd3ff9cba572a75a203ec8b17a36b13653bfef
kbetker <kevin@betker.org>
2021-09-05 18:33:18 -0500

    made decent progress on the new map. close to being done

--- frontend/src/components/SpotNew/SpotNew.js
+++ frontend/src/components/SpotNew/SpotNew.js
@@ -20,12 +20,23 @@ export default function SpotNew() {
     const [mainPic, setMainPic] = useState('');
     const [allowsFamiliar, setAllowsFamiliar] = useState(false);
     const direction = useRef('')
+    const theySeeMeScrolling = useRef('')
+    const leftButton = useRef()
 
     const dispatch = useDispatch();
     const userId = useSelector(state => state.session.user)
     const mapControl = useSelector(state => state.mapControl)
+    const coordinates = useSelector(state => state.coordinates)
 
+    // useEffect(()=>{
+    //     leftButton.current.addEventListener("click", ()=>{
+    //         theySeeMeScrolling.current = setInterval(() => {
+    //             direction.current = "left"
+    //             adjustMap([{ "scale": 0.5 }])
+    //         }, 1000);
+    //     })
 
+    // }, [])
 
     if (!userId) { return null; }
     const payload = {
@@ -48,36 +59,103 @@ export default function SpotNew() {
     }
 
 
+    const initialState = {
+        scale: 1,
+        mapX: 0,
+        mapY: 0,
+        offsetX: 0,
+        offsetY: 0,
+        fontSize: 12,
+        padding: 5,
+        shadowX: 5,
+        shadowY: 5,
+        shadowBlur: 9,
+        maxMapX: 0,
+        maxMapY: -200,
+        minMapX: 0,
+        minMapY: 0,
+      }
+
+
     function adjustMap(arr) {
         let newObj = Object.assign({}, mapControl);
-
+        console.log(newObj)
         for (let i = 0; i < arr.length; i++) {
             let key = Object.keys(arr[i])[0]
             let value = newObj[Object.keys(arr[i])]
             let newValue = Object.values(arr[i])[0]
-            console.log(key,  value + newValue)
+            // console.log(key, newValue, direction.current)
+            if(key === "scale" && (newObj.scale + newValue < 1 || newObj.scale + newValue > 4)) return;
 
-            if(direction.current === "right" && value + newValue <= newObj.maxMapX) {
+            if (direction.current === "right" && value + newValue <= newObj.maxMapX && key === "mapX") {
                 newObj[key] = newObj.maxMapX
-            } else if (direction.current === "down" && value + newValue <= newObj.maxMapY) {
-                 newObj[key] = newObj.maxMapY
-            } else if (direction.current === "left" && value + newValue >=  newObj.minMapX){
+            } else if (direction.current === "down" && value + newValue <= newObj.maxMapY && key === "mapY") {
+                newObj[key] = newObj.maxMapY
+            } else if (direction.current === "left" && value + newValue >= newObj.minMapX && key === "mapX") {
                 newObj[key] = newObj.minMapX
-            } else if (direction.current === "up" && value + newValue >= newObj.minMapY){
+            } else if (direction.current === "up" && value + newValue >= newObj.minMapY && key === "mapY") {
                 newObj[key] = newObj.minMapY
+            }
+
+            else if (key === "mapX" && direction.current === "zoomIn") {
+                let zoomInValX = newObj.minMapX - (coordinates.X * newObj.scale) + 350;
+
+                // if ((zoomInValX) > newObj.minMapX) {
+                //     newObj[key] = newObj.minMapX
+                // } else if ((zoomInValX) < newObj.maxMapX) {
+                //     newObj[key] = newObj.maxMapX
+                // } else {
+                    newObj[key] = zoomInValX
+                // }
+            }
+            else if (key === "mapY" && direction.current === "zoomIn") {
+                let zoomInValY = newObj.minMapY - (coordinates.Y * newObj.scale) + 250;
+
+                // if ((zoomInValY) < newObj.minMapY) {
+                //     newObj[key] = newObj.minMapY
+                // } else if ((zoomInValY) > newObj.maxMapY) {
+                //     newObj[key] = newObj.maxMapY
+                // } else {
+                    newObj[key] = zoomInValY
+                // }
+            }
+
+            else if (key === "mapX" && direction.current === "zoomOut") {
+                if(newObj.scale === 1){
+                    newObj[key] = newObj.minMapX
+                } else {
+                    newObj[key] = newObj.minMapX - (coordinates.X * newObj.scale) + 350;
+                }
+            }
+            else if (key === "mapY" && direction.current === "zoomOut") {
+                if(newObj.scale === 1){
+                    newObj[key] = newObj.minMapY
+                } else {
+                    newObj[key] = newObj.minMapY - (coordinates.Y * newObj.scale) + 250;
+                }
             }
 
             else {
                 newObj[key] += newValue
 
             }
-
-            // console.log(key, value, newValue)
-            // console.log([Object.keys(arr[i])])
-            // newObj[Object.keys(arr[i])] += Object.values(arr[i])[0]
         }
         dispatch(dispatchMapControl(newObj))
+        console.log(newObj)
+    }
 
+    function zoom(xy, min, max) {
+        console.log(mapControl.maxMapX, mapControl.maxMapY)
+
+        let X = (max - min) / 2
+        let Y = (max - min) / 2
+        // let X = mapControl.maxMapX + ((mapControl.maxMapX - mapControl.mapX) / 2);
+        // let Y = mapControl.maxMapY +((mapControl.maxMapY - mapControl.mapY) / 2);
+        if (xy === "x") {
+            return 0
+        } else {
+            return 570
+        }
     }
 
 
@@ -124,60 +202,76 @@ export default function SpotNew() {
                     </form>
                 </div>
                 <div className="mapControls">
-                    <div className="controlElement" onClick={() =>
-                        [direction.current = "left", adjustMap([{ "mapX": 130 }])]
-                        }>
+                    <div className="controlElement"
+                        // ref={leftButton}
+                    onClick={() =>   [direction.current = "left", adjustMap([{ "mapX": 160 }])]}
+                        // [direction.current = "left", adjustMap([{ "mapX": 160 }])]
+                    // }
+                    >
                         <div className="arrowLeft"></div>
                     </div>
 
                     <div className="controlElement" onClick={() =>
-                        [direction.current = "right", adjustMap([{ "mapX": -130 }])]
-                        }>
+                        [direction.current = "right", adjustMap([{ "mapX": -160 }])]
+                    }>
                         <div className="arrowRight" ></div>
                     </div>
 
                     <div className="controlElement" onClick={() =>
-                        [direction.current = "up", adjustMap([{ "mapY": 130 }])]
-                        }>
+                        [direction.current = "up", adjustMap([{ "mapY": 160 }])]
+                    }>
                         <div className="arrowUp"></div>
                     </div>
 
                     <div className="controlElement" onClick={() =>
-                        [direction.current = "down", adjustMap([{ "mapY": -130 }])]
-                        }>
+                        [direction.current = "down", adjustMap([{ "mapY": -160 }])]
+                    }>
                         <div className="arrowDown"></div>
                     </div>
 
-                    <div className="controlElement" onClick={() => adjustMap([
-                        { "scale": 0.5 },
-                        { "offsetX": -175 },
-                        { "offsetY": -700 },
-                        { "fontSize": -2 },
-                        { "padding": -1 },
-                        { "shadowX": -1 },
-                        { "shadowY": -1 },
-                        { "shadowBlur": -1 },
-                        { "maxMapX": -290 },
-                        { "maxMapY": 235 },
-                        { "minMapX": 175 },
-                        { "minMapY": 700 },
-                    ])}>+</div>
-                    <div className="controlElement" onClick={() => adjustMap([{
-                        "scale": -0.5 },
-                        { "offsetX": 175 },
-                        { "offsetY": 700 },
-                        { "fontSize": 2 },
-                        { "padding": 1 },
-                        { "shadowX": 1 },
-                        { "shadowY": 1 },
-                        { "shadowBlur": 1 },
-                        { "maxMapX": 290 },
-                        { "maxMapY": -235 },
-                        { "minMapX": -175 },
-                        { "minMapY": -700 },
-                    ])}>-</div>
-                </div>
 
+
+
+                    <div className="controlElement" onClick={() => [
+                        direction.current = "zoomIn",
+                        adjustMap([
+                            { "scale": 0.5 },
+                            { "offsetX": -175 },
+                            { "offsetY": -700 },
+                            { "fontSize": -1 },
+                            { "padding": -0.5 },
+                            { "shadowX": -1 },
+                            { "shadowY": -1 },
+                            { "shadowBlur": -1 },
+                            { "maxMapX": -175 },
+                            { "maxMapY": 350 },
+                            { "minMapX": 175 },
+                            { "minMapY": 700 },
+                            { "mapX": null },
+                            { "mapY": null },
+                        ])]}>+</div>
+                    <div className="controlElement" onClick={() => [
+                        direction.current = "zoomOut",
+                        adjustMap([
+                            { "scale": -0.5 },
+                            { "offsetX": 175 },
+                            { "offsetY": 700 },
+                            { "fontSize": 1 },
+                            { "padding": 0.5 },
+                            { "shadowX": 1 },
+                            { "shadowY": 1 },
+                            { "shadowBlur": 1 },
+                            { "maxMapX": 175 },
+                            { "maxMapY": -350 },
+                            { "minMapX": -175 },
+                            { "minMapY": -700 },
+                            { "mapX": null },
+                            { "mapY": null },
+                        ])]}>-</div>
+
+                <div className="controlElement resetView" onClick={() => dispatch(dispatchMapControl(initialState))}>reset view</div>
+
+                </div>
                 <div className="map" style={{ overflow: "hidden" }}>
                     <div style={{
                         position: "relative",
--- frontend/src/components/Swordcoast/Swordcoast.js
+++ frontend/src/components/Swordcoast/Swordcoast.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef, useState } from "react"
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { dispatchCoordinates } from "../../store/locCoordinates";
 
 function Swordcoast() {
 
@@ -9,7 +10,7 @@ function Swordcoast() {
   const getXY = useRef()
   const FindPosition = useRef('')
   const GetCoordinates = useRef('')
-
+  const dispatch = useDispatch()
   const [coordinateX, setCoordinateX] = useState(0);
   const [coordinateY, setCoordinateY] = useState(0);
   // const [price, setPrice] = useState(0);
@@ -64,21 +65,38 @@ function Swordcoast() {
           PosY = ((PosY - ImgPos[1]) / mapControl.scale);
           setCoordinateX(PosX)
           setCoordinateY(PosY)
-          console.log(`X:${PosX}, offsetX:${ImgPos[0]}, Y:${PosY}, offsetY:${ImgPos[1]}`)
+           dispatch(dispatchCoordinates({"X": PosX, "Y": PosY}))
+          // console.log(`X:${PosX}, offsetX:${ImgPos[0]}, Y:${PosY}, offsetY:${ImgPos[1]}`)
         }
       },[mapControl])
 
 
   return (
-    <div style={{ width: "2800px", height: "2800px" }}>
+    <div className="mapContainer">
       <img src="/images/swordCoast2800.jpg" ref={getXY} className="swordCoastMap" alt="sword coast map" ></img>
+
       <div id="locOnMap" draggable="true" style={{
         top: coordinateY,
         left: coordinateX,
         fontSize: `${mapControl.fontSize}px`,
         padding: `${mapControl.padding}px`,
-        boxShadow: `${mapControl.shadowX}px ${mapControl.shadowY}px ${mapControl.shadowBlur}px rgba(0, 0, 0, 0.6)` }
-        }>{title}</div>
+        boxShadow: `${mapControl.shadowX}px ${mapControl.shadowY}px ${mapControl.shadowBlur}px rgba(0, 0, 0, 0.6)`,
+        zIndex: "10",
+      }
+        }>{title}
+        <div className="locationDot" style={{
+          width: "10px",
+          height: "10px",
+          top: "-7px",
+          left: "-7px",
+          border: "solid white 2px",
+          zIndex: "-100"
+
+        }}>
+          <div className="square"></div>
+
+        </div>
+        </div>
     </div>
   )
 }
